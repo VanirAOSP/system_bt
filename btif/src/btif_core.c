LineageOS/android_system_bt

bt: support realtek 8723bs
Change-Id: Ib2cec34aacad0a7f1915c21a226c8facc16ac220

btif/src/btif_core.c
@@ -1209,6 +1209,10 @@ bt_status_t btif_set_adapter_property(const bt_property_t *property)
                 BTA_DmSetDeviceName((char *)bd_name);
 
                 storage_req_id = BTIF_CORE_STORAGE_ADAPTER_WRITE;
+#ifdef HCI_USE_RTK_H5
+                btif_config_set_str("Adapter", "Name",(char *)bd_name);
+                btif_config_save();
+#endif
             }
             break;
 

device/src/controller.c
@@ -192,6 +192,24 @@ static future_t *start_up(void) {
 
   // Done telling the controller about what page 0 features we support
   // Request the remaining feature pages
+#ifdef HCI_USE_RTK_H5
+  if(HCI_LMP_EXTENDED_SUPPORTED(features_classic[0].as_array))
+  {
+	  while (page_number < MAX_FEATURES_CLASSIC_PAGE_COUNT) {
+	    response = AWAIT_COMMAND(packet_factory->make_read_local_extended_features(page_number));
+	    packet_parser->parse_read_local_extended_features_response(
+	      response,
+	      &page_number,
+	      &last_features_classic_page_index,
+	      features_classic,
+	      MAX_FEATURES_CLASSIC_PAGE_COUNT
+	    );
+		if(page_number == last_features_classic_page_index) //max page number == current page number,then break;
+			break;
+		page_number++; //otherwise��read next page
+	  }
+  }
+#else
   while (page_number <= last_features_classic_page_index &&
          page_number < MAX_FEATURES_CLASSIC_PAGE_COUNT) {
     response = AWAIT_COMMAND(packet_factory->make_read_local_extended_features(page_number));
@@ -205,6 +223,7 @@ static future_t *start_up(void) {
 
     page_number++;
   }
+#endif
 #if (BLE_INCLUDED == TRUE)
   // read BLE offload features support from controller
   response = AWAIT_COMMAND(packet_factory->make_ble_read_offload_features_support());
@@ -357,14 +376,32 @@ static uint8_t *get_local_supported_codecs(uint8_t *number_of_codecs) {
 
 static const bt_device_features_t *get_features_ble(void) {
   assert(readable);
+#ifdef HCI_USE_RTK_H5
+  if(ble_supported){
+    assert(ble_supported);
+    return &features_ble;
+  } else {
+    return NULL;
+  }
+#else
   assert(ble_supported);
   return &features_ble;
+#endif
 }
 
 static const uint8_t *get_ble_supported_states(void) {
   assert(readable);
+#ifdef HCI_USE_RTK_H5
+  if(ble_supported){
+    assert(ble_supported);
+    return ble_supported_states;
+  } else {
+    return NULL;
+  }
+#else
   assert(ble_supported);
   return ble_supported_states;
+#endif
 }
 
 static bool supports_simple_pairing(void) {
@@ -443,8 +480,18 @@ static uint16_t get_acl_data_size_classic(void) {
 
 static uint16_t get_acl_data_size_ble(void) {
   assert(readable);
+#ifdef HCI_USE_RTK_H5
+  if(ble_supported){
+    assert(ble_supported);
+    return acl_data_size_ble;
+  } else {
+    return 0;
+  }
+#else
   assert(ble_supported);
   return acl_data_size_ble;
+#endif
+
 }
 
 static uint16_t get_acl_packet_size_classic(void) {
@@ -459,8 +506,17 @@ static uint16_t get_acl_packet_size_ble(void) {
 
 static uint16_t get_ble_suggested_default_data_length(void) {
   assert(readable);
+#ifdef HCI_USE_RTK_H5
+  if(ble_supported){
+    assert(ble_supported);
+    return ble_suggested_default_data_length;
+  } else {
+    return 0;
+  }
+#else
   assert(ble_supported);
   return ble_suggested_default_data_length;
+#endif
 }
 
 static uint16_t get_acl_buffer_count_classic(void) {
@@ -476,14 +532,32 @@ static uint8_t get_acl_buffer_count_ble(void) {
 
 static uint8_t get_ble_white_list_size(void) {
   assert(readable);
+#ifdef HCI_USE_RTK_H5
+  if(ble_supported){
+    assert(ble_supported);
+    return ble_white_list_size;
+  } else {
+    return 0;
+  }
+#else
   assert(ble_supported);
   return ble_white_list_size;
+#endif
 }
 
 static uint8_t get_ble_resolving_list_max_size(void) {
   assert(readable);
+#ifdef HCI_USE_RTK_H5
+  if(ble_supported){
+    assert(ble_supported);
+    return ble_resolving_list_max_size;
+  } else {
+    return 0;
+  }
+#else
   assert(ble_supported);
   return ble_resolving_list_max_size;
+#endif
 }
 
 static void set_ble_resolving_list_max_size(int resolving_list_max_size) {
@@ -492,8 +566,12 @@ static void set_ble_resolving_list_max_size(int resolving_list_max_size) {
   if (resolving_list_max_size != 0) {
     assert(readable);
   }
+#ifdef HCI_USE_RTK_H5
+  ble_resolving_list_max_size = resolving_list_max_size;
+#else
   assert(ble_supported);
   ble_resolving_list_max_size = resolving_list_max_size;
+#endif
 }
 
 static const controller_static_t static_interface = {

hci/include/hci_layer.h
@@ -26,6 +26,9 @@
 #include "osi/include/future.h"
 #include "osi/include/osi.h"
 #include "bt_types.h"
+#ifdef HCI_USE_RTK_H5
+#include "bt_hci_bdroid.h"
+#endif
 
 static const char HCI_MODULE[] = "hci_module";
 
@@ -60,7 +63,14 @@ typedef struct packet_fragmenter_t packet_fragmenter_t;
 typedef struct vendor_t vendor_t;
 typedef struct low_power_manager_t low_power_manager_t;
 
+#ifdef HCI_USE_RTK_H5
+/* BD Address */
+typedef struct {
+    uint8_t b[6];
+} __packed bdaddr_t;
+#else
 typedef unsigned char * bdaddr_t;
+#endif
 typedef uint16_t command_opcode_t;
 
 typedef enum {
@@ -73,6 +83,53 @@ typedef enum {
 typedef void (*command_complete_cb)(BT_HDR *response, void *context);
 typedef void (*command_status_cb)(uint8_t status, BT_HDR *command, void *context);
 
+#ifdef HCI_USE_RTK_H5
+typedef void (* p_callback)(void *p_mem);
+
+/******************************************************************************
+**  Constants & Macros
+******************************************************************************/
+
+/******************************************************************************
+**  Type definitions
+******************************************************************************/
+
+/** Prototypes for HCI Service interface functions **/
+/* Callback function for the returned event of internally issued command */
+typedef void (*tINT_CMD_CBACK)(void *p_mem);
+
+/* Initialize transport's control block */
+typedef void (*tHCI_INIT)(tINT_CMD_CBACK p_cback, const allocator_t *bufalloc);
+
+/* Do transport's control block clean-up */
+typedef void (*tHCI_CLEANUP)(void);
+
+/* Send HCI command/data to the transport */
+typedef void (*tHCI_SEND)(HC_BT_HDR *p_msg);
+
+/* Handler for HCI upstream path */
+typedef uint16_t (*tHCI_RCV)(uint16_t *byte);
+
+/* Handler for sending HCI command from the local module */
+typedef uint8_t (*tHCI_SEND_INT)(uint16_t opcode, HC_BT_HDR *p_buf, \
+                                  tINT_CMD_CBACK p_cback);
+
+/* Handler for getting acl data length */
+typedef void (*tHCI_ACL_DATA_LEN_HDLR)(void);
+
+/******************************************************************************
+**  Extern variables and functions
+******************************************************************************/
+
+typedef struct {
+    tHCI_INIT init;
+    tHCI_CLEANUP cleanup;
+    tHCI_SEND send;
+    tHCI_SEND_INT send_int_cmd;
+    tHCI_ACL_DATA_LEN_HDLR get_acl_max_len;
+    tHCI_RCV rcv;
+} tHCI_IF;
+#endif
 typedef struct hci_t {
   // Send a low power command, if supported and the low power manager is enabled.
   void (*send_low_power_command)(low_power_command_t command);
@@ -85,6 +142,14 @@ typedef struct hci_t {
 
   // Set the queue to receive ACL data in
   void (*set_data_queue)(fixed_queue_t *queue);
+#ifdef HCI_USE_RTK_H5
+  // Send HCI INT command through the HCI layer
+  void (*transmit_int_command)(
+      uint16_t opcode,
+      void *buffer,
+      p_callback callback
+  );
+#endif
 
   // Send a command through the HCI layer
   void (*transmit_command)(

hci/include/packet_fragmenter.h
@@ -25,6 +25,9 @@
 typedef void (*transmit_finished_cb)(BT_HDR *packet, bool all_fragments_sent);
 typedef void (*packet_reassembled_cb)(BT_HDR *packet);
 typedef void (*packet_fragmented_cb)(BT_HDR *packet, bool send_transmit_finished);
+#ifdef HCI_USE_RTK_H5
+typedef void (*filter_incoming_event_cb)(BT_HDR *packet);
+#endif
 
 typedef struct {
   // Called for every packet fragment.
@@ -36,6 +39,10 @@ typedef struct {
   // Called when the fragmenter finishes sending all requested fragments,
   // but the packet has not been entirely sent.
   transmit_finished_cb transmit_finished;
+#ifdef HCI_USE_RTK_H5
+  //handle the callback of cmd comp event and cmd status event
+  filter_incoming_event_cb filter_incoming_event;
+#endif
 } packet_fragmenter_callbacks_t;
 
 typedef struct packet_fragmenter_t {

hci/src/vendor.c
@@ -94,7 +94,7 @@ void vendor_ssrcleanup(int reason) {
   /* This is horrible. Not all implementations have this method (from ours,
    * only 8992 appears to have it), so never try to call this on vendor
    * libraries without a "complete" interface (since this is the last one) */
-  if (lib_interface && lib_interface->size >= 
+  if (lib_interface && lib_interface->size >=
         (sizeof(bt_vendor_interface_t)))
     lib_interface->ssr_cleanup(reason);
   else
@@ -188,7 +188,11 @@ static void transmit_completed_callback(BT_HDR *response, void *context) {
 // Called back from vendor library when it wants to send an HCI command.
 static uint8_t transmit_cb(UNUSED_ATTR uint16_t opcode, void *buffer, tINT_CMD_CBACK callback) {
   assert(hci != NULL);
+#ifdef HCI_USE_RTK_H5
+  hci->transmit_int_command(opcode, (BT_HDR *)buffer, callback);
+#else
   hci->transmit_command((BT_HDR *)buffer, transmit_completed_callback, NULL, callback);
+#endif
   return true;
 }
 

include/bt_target.h
@@ -400,7 +400,11 @@
 
 /* The default scan mode */
 #ifndef BTM_DEFAULT_SCAN_TYPE
+#ifdef HCI_USE_RTK_H5
+#define BTM_DEFAULT_SCAN_TYPE       BTM_SCAN_TYPE_STANDARD
+#else
 #define BTM_DEFAULT_SCAN_TYPE       BTM_SCAN_TYPE_INTERLACED
+#endif
 #endif
 
 /* Should connections to unknown devices be allowed when not discoverable? */
@@ -1626,7 +1630,11 @@ The maximum number of payload octets that the local device can receive in a sing
 ******************************************************************************/
 
 #ifndef HCILP_INCLUDED
+#ifdef HCI_USE_RTK_H5
+#define HCILP_INCLUDED                  FALSE
+#else
 #define HCILP_INCLUDED                  TRUE
+#endif
 #endif
 
 /******************************************************************************

stack/rfcomm/rfc_int.h
@@ -206,7 +206,11 @@ typedef struct
 #define RFC_T1_TIMEOUT                  20   /* seconds to wait for reply with Poll bit */
 #define RFC_PORT_T1_TIMEOUT             60   /* seconds to wait for reply with Poll bit other than MX */
 #define RFC_T2_TIMEOUT                  20   /* timeout to wait for Mx UIH */
+#ifdef HCI_USE_RTK_H5
+#define RFC_DISC_TIMEOUT                30    /* If something goes wrong and we send DISC we should not wait for min */
+#else
 #define RFC_DISC_TIMEOUT                3    /* If something goes wrong and we send DISC we should not wait for min */
+#endif
 #define RFC_CLOSE_TIMEOUT               10
 #define RFCOMM_CONN_TIMEOUT            120   /* first connection to be established on Mx */
 

VanirAOSP/system_bt

Change-Id: I1484be980236a20e64b0ca21b14b13ca96e22b6a
Signed-off-by: DHOMD <DHO@dho.im>

btif/src/btif_core.c
@@ -1209,6 +1209,10 @@ bt_status_t btif_set_adapter_property(const bt_property_t *property)
                 BTA_DmSetDeviceName((char *)bd_name);
 
                 storage_req_id = BTIF_CORE_STORAGE_ADAPTER_WRITE;
+#ifdef BLUETOOTH_RTK
+                btif_config_set_str("Adapter", "Name",(char *)bd_name);
+                btif_config_save();
+#endif
             }
             break;
 

device/src/controller.c
@@ -192,6 +192,24 @@ static future_t *start_up(void) {
 
   // Done telling the controller about what page 0 features we support
   // Request the remaining feature pages
+#ifdef BLUETOOTH_RTK
+  if(HCI_LMP_EXTENDED_SUPPORTED(features_classic[0].as_array))
+  {
+	  while (page_number < MAX_FEATURES_CLASSIC_PAGE_COUNT) {
+	    response = AWAIT_COMMAND(packet_factory->make_read_local_extended_features(page_number));
+	    packet_parser->parse_read_local_extended_features_response(
+	      response,
+	      &page_number,
+	      &last_features_classic_page_index,
+	      features_classic,
+	      MAX_FEATURES_CLASSIC_PAGE_COUNT
+	    );
+		if(page_number == last_features_classic_page_index) //max page number == current page number,then break;
+			break;
+		page_number++; //otherwise��read next page
+	  }
+  }
+#else
   while (page_number <= last_features_classic_page_index &&
          page_number < MAX_FEATURES_CLASSIC_PAGE_COUNT) {
     response = AWAIT_COMMAND(packet_factory->make_read_local_extended_features(page_number));
@@ -205,6 +223,7 @@ static future_t *start_up(void) {
 
     page_number++;
   }
+#endif
 #if (BLE_INCLUDED == TRUE)
   // read BLE offload features support from controller
   response = AWAIT_COMMAND(packet_factory->make_ble_read_offload_features_support());
@@ -357,14 +376,32 @@ static uint8_t *get_local_supported_codecs(uint8_t *number_of_codecs) {
 
 static const bt_device_features_t *get_features_ble(void) {
   assert(readable);
+#ifdef BLUETOOTH_RTK
+  if(ble_supported){
+    assert(ble_supported);
+    return &features_ble;
+  } else {
+    return NULL;
+  }
+#else
   assert(ble_supported);
   return &features_ble;
+#endif
 }
 
 static const uint8_t *get_ble_supported_states(void) {
   assert(readable);
+#ifdef BLUETOOTH_RTK
+  if(ble_supported){
+    assert(ble_supported);
+    return ble_supported_states;
+  } else {
+    return NULL;
+  }
+#else
   assert(ble_supported);
   return ble_supported_states;
+#endif
 }
 
 static bool supports_simple_pairing(void) {
@@ -443,8 +480,18 @@ static uint16_t get_acl_data_size_classic(void) {
 
 static uint16_t get_acl_data_size_ble(void) {
   assert(readable);
+#ifdef BLUETOOTH_RTK
+  if(ble_supported){
+    assert(ble_supported);
+    return acl_data_size_ble;
+  } else {
+    return 0;
+  }
+#else
   assert(ble_supported);
   return acl_data_size_ble;
+#endif
+
 }
 
 static uint16_t get_acl_packet_size_classic(void) {
@@ -459,8 +506,17 @@ static uint16_t get_acl_packet_size_ble(void) {
 
 static uint16_t get_ble_suggested_default_data_length(void) {
   assert(readable);
+#ifdef BLUETOOTH_RTK
+  if(ble_supported){
+    assert(ble_supported);
+    return ble_suggested_default_data_length;
+  } else {
+    return 0;
+  }
+#else
   assert(ble_supported);
   return ble_suggested_default_data_length;
+#endif
 }
 
 static uint16_t get_acl_buffer_count_classic(void) {
@@ -476,14 +532,32 @@ static uint8_t get_acl_buffer_count_ble(void) {
 
 static uint8_t get_ble_white_list_size(void) {
   assert(readable);
+#ifdef BLUETOOTH_RTK
+  if(ble_supported){
+    assert(ble_supported);
+    return ble_white_list_size;
+  } else {
+    return 0;
+  }
+#else
   assert(ble_supported);
   return ble_white_list_size;
+#endif
 }
 
 static uint8_t get_ble_resolving_list_max_size(void) {
   assert(readable);
+#ifdef BLUETOOTH_RTK
+  if(ble_supported){
+    assert(ble_supported);
+    return ble_resolving_list_max_size;
+  } else {
+    return 0;
+  }
+#else
   assert(ble_supported);
   return ble_resolving_list_max_size;
+#endif
 }
 
 static void set_ble_resolving_list_max_size(int resolving_list_max_size) {
@@ -492,8 +566,12 @@ static void set_ble_resolving_list_max_size(int resolving_list_max_size) {
   if (resolving_list_max_size != 0) {
     assert(readable);
   }
+#ifdef BLUETOOTH_RTK
+  ble_resolving_list_max_size = resolving_list_max_size;
+#else
   assert(ble_supported);
   ble_resolving_list_max_size = resolving_list_max_size;
+#endif
 }
 
 static const controller_static_t static_interface = {

hci/include/hci_layer.h
@@ -26,6 +26,9 @@
 #include "osi/include/future.h"
 #include "osi/include/osi.h"
 #include "bt_types.h"
+#ifdef BLUETOOTH_RTK
+#include "bt_hci_bdroid.h"
+#endif
 
 static const char HCI_MODULE[] = "hci_module";
 
@@ -60,7 +63,14 @@ typedef struct packet_fragmenter_t packet_fragmenter_t;
 typedef struct vendor_t vendor_t;
 typedef struct low_power_manager_t low_power_manager_t;
 
+#ifdef BLUETOOTH_RTK
+/* BD Address */
+typedef struct {
+    uint8_t b[6];
+} __packed bdaddr_t;
+#else
 typedef unsigned char * bdaddr_t;
+#endif
 typedef uint16_t command_opcode_t;
 
 typedef enum {
@@ -73,6 +83,53 @@ typedef enum {
 typedef void (*command_complete_cb)(BT_HDR *response, void *context);
 typedef void (*command_status_cb)(uint8_t status, BT_HDR *command, void *context);
 
+#ifdef BLUETOOTH_RTK
+typedef void (* p_callback)(void *p_mem);
+
+/******************************************************************************
+**  Constants & Macros
+******************************************************************************/
+
+/******************************************************************************
+**  Type definitions
+******************************************************************************/
+
+/** Prototypes for HCI Service interface functions **/
+/* Callback function for the returned event of internally issued command */
+typedef void (*tINT_CMD_CBACK)(void *p_mem);
+
+/* Initialize transport's control block */
+typedef void (*tHCI_INIT)(tINT_CMD_CBACK p_cback, const allocator_t *bufalloc);
+
+/* Do transport's control block clean-up */
+typedef void (*tHCI_CLEANUP)(void);
+
+/* Send HCI command/data to the transport */
+typedef void (*tHCI_SEND)(HC_BT_HDR *p_msg);
+
+/* Handler for HCI upstream path */
+typedef uint16_t (*tHCI_RCV)(uint16_t *byte);
+
+/* Handler for sending HCI command from the local module */
+typedef uint8_t (*tHCI_SEND_INT)(uint16_t opcode, HC_BT_HDR *p_buf, \
+                                  tINT_CMD_CBACK p_cback);
+
+/* Handler for getting acl data length */
+typedef void (*tHCI_ACL_DATA_LEN_HDLR)(void);
+
+/******************************************************************************
+**  Extern variables and functions
+******************************************************************************/
+
+typedef struct {
+    tHCI_INIT init;
+    tHCI_CLEANUP cleanup;
+    tHCI_SEND send;
+    tHCI_SEND_INT send_int_cmd;
+    tHCI_ACL_DATA_LEN_HDLR get_acl_max_len;
+    tHCI_RCV rcv;
+} tHCI_IF;
+#endif
 typedef struct hci_t {
   // Send a low power command, if supported and the low power manager is enabled.
   void (*send_low_power_command)(low_power_command_t command);
@@ -85,6 +142,14 @@ typedef struct hci_t {
 
   // Set the queue to receive ACL data in
   void (*set_data_queue)(fixed_queue_t *queue);
+#ifdef BLUETOOTH_RTK
+  // Send HCI INT command through the HCI layer
+  void (*transmit_int_command)(
+      uint16_t opcode,
+      void *buffer,
+      p_callback callback
+  );
+#endif
 
   // Send a command through the HCI layer
   void (*transmit_command)(

hci/include/packet_fragmenter.h
@@ -25,6 +25,9 @@
 typedef void (*transmit_finished_cb)(BT_HDR *packet, bool all_fragments_sent);
 typedef void (*packet_reassembled_cb)(BT_HDR *packet);
 typedef void (*packet_fragmented_cb)(BT_HDR *packet, bool send_transmit_finished);
+#ifdef BLUETOOTH_RTK
+typedef void (*filter_incoming_event_cb)(BT_HDR *packet);
+#endif
 
 typedef struct {
   // Called for every packet fragment.
@@ -36,6 +39,10 @@ typedef struct {
   // Called when the fragmenter finishes sending all requested fragments,
   // but the packet has not been entirely sent.
   transmit_finished_cb transmit_finished;
+#ifdef BLUETOOTH_RTK
+  //handle the callback of cmd comp event and cmd status event
+  filter_incoming_event_cb filter_incoming_event;
+#endif
 } packet_fragmenter_callbacks_t;
 
 typedef struct packet_fragmenter_t {

hci/src/vendor.c
@@ -94,7 +94,7 @@ void vendor_ssrcleanup(int reason) {
   /* This is horrible. Not all implementations have this method (from ours,
    * only 8992 appears to have it), so never try to call this on vendor
    * libraries without a "complete" interface (since this is the last one) */
-  if (lib_interface && lib_interface->size >= 
+  if (lib_interface && lib_interface->size >=
         (sizeof(bt_vendor_interface_t)))
     lib_interface->ssr_cleanup(reason);
   else
@@ -188,7 +188,11 @@ static void transmit_completed_callback(BT_HDR *response, void *context) {
 // Called back from vendor library when it wants to send an HCI command.
 static uint8_t transmit_cb(UNUSED_ATTR uint16_t opcode, void *buffer, tINT_CMD_CBACK callback) {
   assert(hci != NULL);
+#ifdef BLUETOOTH_RTK
+  hci->transmit_int_command(opcode, (BT_HDR *)buffer, callback);
+#else
   hci->transmit_command((BT_HDR *)buffer, transmit_completed_callback, NULL, callback);
+#endif
   return true;
 }
 

include/bt_target.h
@@ -399,7 +399,11 @@
 
 /* The default scan mode */
 #ifndef BTM_DEFAULT_SCAN_TYPE
+#ifdef BLUETOOTH_RTK
+#define BTM_DEFAULT_SCAN_TYPE       BTM_SCAN_TYPE_STANDARD
+#else
 #define BTM_DEFAULT_SCAN_TYPE       BTM_SCAN_TYPE_INTERLACED
+#endif
 #endif
 
 /* Should connections to unknown devices be allowed when not discoverable? */
@@ -1625,7 +1629,11 @@ The maximum number of payload octets that the local device can receive in a sing
 ******************************************************************************/
 
 #ifndef HCILP_INCLUDED
+#ifdef BLUETOOTH_RTK
+#define HCILP_INCLUDED                  FALSE
+#else
 #define HCILP_INCLUDED                  TRUE
+#endif
 #endif
 
 /******************************************************************************

stack/rfcomm/rfc_int.h
@@ -206,7 +206,11 @@ typedef struct
 #define RFC_T1_TIMEOUT                  20   /* seconds to wait for reply with Poll bit */
 #define RFC_PORT_T1_TIMEOUT             60   /* seconds to wait for reply with Poll bit other than MX */
 #define RFC_T2_TIMEOUT                  20   /* timeout to wait for Mx UIH */
+#ifdef BLUETOOTH_RTK
+#define RFC_DISC_TIMEOUT                30    /* If something goes wrong and we send DISC we should not wait for min */
+#else
 #define RFC_DISC_TIMEOUT                3    /* If something goes wrong and we send DISC we should not wait for min */
+#endif
 #define RFC_CLOSE_TIMEOUT               10
 #define RFCOMM_CONN_TIMEOUT            120   /* first connection to be established on Mx */
 